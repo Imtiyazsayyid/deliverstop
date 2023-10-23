6a23d13f2079ba68bb67badc68b1522ff8b9ce53
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2023-10-23 17:12:33 +0530

    configure new default font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Button, Text } from "@radix-ui/themes";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <main>
+      <Text className="text-4xl font-light">Hello</Text>
+      <Button>Hello</Button>
+    </main>
+  );
 }
